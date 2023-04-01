b89ca063a5c651582b8b062293ae0bcd839ce0e3
root <root@Grogoller2>
2023-04-01 02:34:45 +0100

    Auto-sync commit

--- generate_response.py
+++ generate_response.py
@@ -36,3 +36,49 @@ def ensemble_predictions(predictions, weights):
 
     ensemble_prediction = sum(prediction * weight for prediction, weight in zip(predictions, weights)) / sum(weights)
     return ensemble_prediction
+
+
+'''
+каким образом теперь лучше всего включить код с translation_models.py в generate_response.py
+предложи обновленный вариант код, с улучшениями, но важно, без потери функциональности предыдущего, предложи готовый код с комментариями на русском языке
+
+from transformers import pipeline
+from load_models import load_main_model
+from text_processing import clean_text, validate_input
+from text_generator import text_generator
+
+def generate_response_with_pipeline(model_name, user_prompt, num_beams=5, temperature=1.0, **kwargs):
+    model, tokenizer = load_main_model(model_name)
+    generator = pipeline('text-generation', model=model, tokenizer=tokenizer, device=0, num_beams=num_beams, max_length=1024, temperature=temperature, **kwargs)
+    response = generator(user_prompt)[0]['generated_text']
+    return response
+
+def generate_response(user_prompt: str, model_name: str, ensemble: bool = False,
+                      back_translate: bool = False, num_beams=5, temperature=1.0,
+                      use_encoder_decoder: bool = False, **kwargs):
+    # Validate input
+    if not validate_input(user_prompt):
+        return "Пожалуйста, введите корректный текст."
+
+    # Clean text
+    cleaned_text = clean_text(user_prompt)
+
+    if use_encoder_decoder:
+        response = text_generator(cleaned_text)
+    else:
+        response = generate_response_with_pipeline(model_name, cleaned_text, num_beams, temperature, **kwargs)
+
+    return response
+
+def ensemble_predictions(predictions, weights):
+    if len(predictions) != len(weights):
+        raise ValueError("Количество предсказаний и весов должно совпадать")
+
+    for weight in weights:
+        if weight < 0:
+            raise ValueError("Веса должны быть неотрицательными")
+
+    ensemble_prediction = sum(prediction * weight for prediction, weight in zip(predictions, weights)) / sum(weights)
+    return ensemble_prediction
+
+'''
---
+++ translation_models.py
@@ -0,0 +1,46 @@
+import torch
+import transformers
+from config import DEVICE, TRANSLATION_MAX_LENGTH, BACK_TRANSLATION_MODEL_NAME, TRANSLATION_MODEL_NAME
+from typing import List, Tuple
+
+class TranslationModel:
+    def __init__(self, model_name: str, device: torch.device = None):
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+        self.model = transformers.AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
+        self.device = device or torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = self.model.to(self.device)
+
+    def translate(self, text: str, max_length: int = TRANSLATION_MAX_LENGTH) -> str:
+        inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=max_length)
+        input_ids = inputs["input_ids"].to(self.device)
+        attention_mask = inputs["attention_mask"].to(self.device)
+        outputs = self.model.generate(input_ids=input_ids, attention_mask=attention_mask, max_length=max_length)
+        decoded = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return decoded
+
+    def batch_translate(self, texts: List[str], batch_size: int = 32) -> List[str]:
+        batches = [texts[i:i+batch_size] for i in range(0, len(texts), batch_size)]
+        results = []
+        for batch in batches:
+            input_ids = self.tokenizer.batch_encode_plus(batch, return_tensors="pt").to(self.device)
+            outputs = self.model.generate(input_ids["input_ids"])
+            decoded = [self.tokenizer.decode(outputs[i], skip_special_tokens=True) for i in range(len(batch))]
+            results += decoded
+        return results
+
+    def back_translate(self, text: str) -> str:
+        # Перевод текста на английский язык
+        english_text = self.translate(text, TRANSLATION_MAX_LENGTH)
+
+        # Обратный перевод английского текста на исходный язык
+        back_translated_text = self.translate(english_text, TRANSLATION_MAX_LENGTH)
+
+        return back_translated_text
+
+    def close(self):
+        self.model.cpu()
+        torch.cuda.empty_cache()
+
+# ...
+
+# Ваши другие функции и код для ансамблей моделей, если они нужны.
